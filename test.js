61d8005b6d4e564435a388f837916cef4df42079
demianov-an <Alexm92@inbox.ru>
2023-12-30 19:38:19 +0600

    tribonacci-sequence

--- test.js
+++
@@ -1,8 +0,0 @@
-function extractCurrencyValue(str) {
-  numberStr = ''
-  for (let n of str) {
-    n.codePointAt(0) >= 48 && n.codePointAt(0) <= 57 ? numberStr += n : ''
-  }
-  return Number(numberStr)
-}
-log
