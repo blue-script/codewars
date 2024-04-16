318a29f38de8315b791109e1a762b0c1dcd43b98
blue-script <alexm92@inbox.ru>
2024-04-16 11:39:43 +0500

    rm unnecessary files

--- editing-file-name.js
+++
@@ -1,2 +0,0 @@
-const text = "Area or Perimeter"
-console.log(text.replaceAll(' ', '-').toLowerCase())
--- test.js
+++
@@ -1,5 +0,0 @@
-let message =  'Invalid url format (Contacts->Facebook)'
-// let res = message.match(/\(.*\)$/)
-let res = message.match(/\((.*?)\)/)
-console.log(res);
-console.log(res.index);
